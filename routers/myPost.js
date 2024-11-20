a3c62d4b1dc9704ef7be0f6a1c58291be90b6c0a
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-11-20 17:00:11 +0100

    fix myPosts page

--- routers/myPost.js
+++ routers/myPost.js
@@ -1,73 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const post = [
-  {
-    Title: "ciambellone",
-    Content: "Ricetta",
-    Image: "http://127.0.0.1:3000/ciambellone.jpeg",
-    Tags: [
-      "stringa1",
-      "stringa2",
-      "stringa3",
-      "stringa4",
-      "stringa5",
-      "stringa6",
-    ],
-  },
-  {
-    Title: "cracker barbabietola",
-    Content: "Ricetta",
-    Image: "http://127.0.0.1:3000/cracker_barbabietola.jpeg",
-    Tags: [
-      "stringa1",
-      "stringa2",
-      "stringa3",
-      "stringa4",
-      "stringa5",
-      "stringa6",
-    ],
-  },
-  {
-    Title: "pane fritto dolce",
-    Content: "Ricetta",
-    Image: "http://127.0.0.1:3000/pane_fritto_dolce.jpeg",
-    Tags: [
-      "stringa1",
-      "stringa2",
-      "stringa3",
-      "stringa4",
-      "stringa5",
-      "stringa6",
-    ],
-  },
-  {
-    Title: "pasta barbabietola",
-    Content: "Ricetta",
-    Image: "http://127.0.0.1:3000/pasta_barbabietola.jpeg",
-    Tags: [
-      "stringa1",
-      "stringa2",
-      "stringa3",
-      "stringa4",
-      "stringa5",
-      "stringa6",
-    ],
-  },
-  {
-    Title: "torta paesana",
-    Content: "Ricetta",
-    Image: "http://127.0.0.1:3000/torta_paesana.jpeg",
-    Tags: [
-      "stringa1",
-      "stringa2",
-      "stringa3",
-      "stringa4",
-      "stringa5",
-      "stringa6",
-    ],
-  },
-];
+const post = require("../db/postArray");
 
 router.get("/", (req, res) => {
   let totalPost = 0;
